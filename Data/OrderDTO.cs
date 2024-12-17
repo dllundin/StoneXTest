176b25de695bdfa58a5eb0b238bcad885640d489
DL <DL@DL-HP-LAP>
2024-12-17 02:56:23 -0600

    Add project files.

---
+++ Data/OrderDTO.cs
@@ -0,0 +1,18 @@
+﻿namespace StoneXTest.Data
+{
+    public class OrderDTO
+    {
+        public int OrderId { get; set; } = 0;
+        public double Price { get; set; }
+        public int Qty { get; set; }
+        public string OrderDate { get; set; }
+        public string Side { get; set; }
+        public int? NumOfOrders { get; set; }
+    }
+
+    public class OrderVM: OrderDTO
+    {
+        
+        
+    }
+}
